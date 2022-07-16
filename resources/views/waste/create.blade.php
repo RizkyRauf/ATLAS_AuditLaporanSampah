c98683ccdd7a64535b2d9b8853eed7247a93b33f
Reyhan1610 <alanareyhan1@gmail.com>
2022-07-16 13:37:19 +0700

    update tampilan halaman tambah data

--- resources/views/waste/create.blade.php
+++ resources/views/waste/create.blade.php
@@ -1,10 +1,7 @@
 @extends("layouts.app")
 @section('wrapper')
 
-<div class="card uper">
-<div class="card-header">
-    Tambah Data Sampah
-</div>
+<br><br><br>
 <div class="card-body">
     <form method="post" action="{{ route('wastes.store') }}">
         <div class="form-group">
@@ -15,27 +12,29 @@
         <div class="form-group">
             @csrf
             <label for="kategori">Kategori:</label>
-            <select id="kategori" name="kategori">
-                <option value="">-- Pilih Ketegori Sampah--</option>
-                <option value="Buah atau Sayuran">Buah atau Sayuran</option>
-                <option value="Barang Elektronik">Barang Elektronik</option>
-                <option value="Kemasan Makanan">Kemasan Makanan</option>
-                <option value="Kemasan Minuman">Kemasan Minuman</option>
-                <option value="Produk Perawatan Diri">Produk Perawatan Diri</option>
-                <option value="Produk Rumah Tangga">Produk Rumah Tangga</option>
-                <option value="Bahan Kimia">Bahan Kimia</option>
+            <select id="kategori" class="form-control" name="kategori">
+                <option value="">-- Pilih Kategori Sampah--</option>
+                <option value="Plastik Daur Ulang">Plastik Daur Ulang</option>
+                <option value="Plastik Sekali Pakai">Plastik Sekali Pakai</option>
+                <option value="Tekstil">Tekstil</option>
+                <option value="Logam">Logam</option>
+                <option value="Kertas">Kertas</option>
+                <option value="Kaca">Kaca</option>
+                <option value="Karet">Karet</option>
+                <option value="Kayu">Kayu</option>
+                <option value="Keramik">Keramik</option>
+                <option value="B3">B3</option>
                 <option value="Lainnya">Lainnya</option>
             </select>
         </div>
         <div class="form-group">
             @csrf
             <label for="jenis_sampah">Jenis Sampah:</label>
-            <select id="jenis_sampah" name="jenis_sampah">
+            <select id="jenis_sampah" class="form-control" name="jenis_sampah">
                 <option value="">-- Pilih Jenis Sampah--</option>
                 <option value="Organik">Organik</option>
                 <option value="Anorganik">Anorganik</option>
                 <option value="B3">B3</option>
-                <option value="Lainnya">Lainnya</option>
             </select>
         </div>
         <div class="form-group">
@@ -45,9 +44,9 @@
         </div>
         <div class="form-group">
             @csrf
-            <label for="berat_sampah">Berat Sampah:</label>
+            <label for="berat_sampah">Berat Sampah:</label><br>
             <input type="number" id="berat_sampah" name="berat_sampah" min="1" max="100"> KG
-        </div>
+        </div><br>
         <button type="submit" class="btn btn-primary">Tambah Data</button>
     </form>
 </div>
